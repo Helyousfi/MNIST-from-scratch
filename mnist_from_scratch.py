91763345dddaeee68bf9c9bf053bb24c88cd3b5b
helyousi <hamza.el_yousfi@bordeaux-inp.fr>
2021-12-06 18:43:31 +0100

    backward propagation

--- mnist_from_scratch.py
+++ mnist_from_scratch.py
@@ -3,15 +3,29 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 
 data = pd.read_csv('train.csv/train.csv')
-data = np.array(data) 
+print(data.head()) 
+data = np.array(data)
 m, n = data.shape
+print(m,n)
 np.random.shuffle(data)
 
+#Splitting the dataset
+trainingSet = data[1000:m].T
+trainY = trainingSet[0]
+trainX = trainingSet[1:n] 
+trainX = trainX / 255
+
+validationSet = data[0:1000].T 
+validationY = validationSet[0]
+validationX = validationSet[1:n]
+validationX = validationX / 255
+
+
 def init_params():
-    W1 = np.random.randn(10, 784) - 0.5;
-    b1 = np.random.randn(10, 1) - 0.5;
-    W2 = np.random.randn(10, 10) - 0.5;
-    b2 = np.random.randn(10, 1) -0.5;
+    W1 = np.random.randn(10, 784)
+    b1 = np.random.randn(10, 1)
+    W2 = np.random.randn(10, 10) 
+    b2 = np.random.randn(10, 1) 
     return W1, b1, W2, b2
 
 def ReLU(Z):
@@ -27,5 +41,9 @@ def forward_propagation(W1, b1, W2, b2, X):
     A2 = softmax(Z2)
     return Z1, A1, Z2, A2
 
-def back_prop(Z1, A1, Z2, A2, W2, Y):
+# 1 => [0 1 0 0 0 0 0 0 0 0]
+def labelToArray(Y):
+    
+
+#def back_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
     
